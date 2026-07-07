085247e44f3a57449ed33fdda1d4b602373110c1
arielamado2do-del <arielamado2do@gmail.com>
2026-07-07 10:43:24 -0900

    arreglando los errores y simplificando

--- inicio.js
+++ inicio.js
@@ -6,13 +6,31 @@ function recuperarD() {
         const art = document.createElement("article");
         const boton = document.createElement("button");
 boton.textContent = "guardar";
+let existe = Mp.some(function(pro){
+    return pro.nombre === P.nombre
+})
 boton.onclick = function() {
-    Mp.push({
+    if(existe) {
+        if(
+confirm("Ya tienes este producto ¿Deseas agregarlo una vez mas?")
+        ) {      Mp.push({
         nombre: P.nombre,
         precio: P.precio,
         descripcion: P.descripcion,
-        categoria: P.categoria
-    });
+        categoria: P.categoria,
+      
+    })}
+}
+else {
+      Mp.push({
+        nombre: P.nombre,
+        precio: P.precio,
+        descripcion: P.descripcion,
+        categoria: P.categoria,
+      
+    })
+}
+
     localStorage.setItem("guardado2", JSON.stringify(Mp));
 }
     art.innerHTML = "<h2>" + P.nombre + "</h2><p>Precio: " + P.precio + "</p><p>Descripción: " + P.descripcion + "</p><p>Categoría: " + P.categoria + "</p>";
